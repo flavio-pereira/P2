d081410e208ef4cfe122ed58b0e325eec68b8937
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-09-29 22:49:59 -0600

    Made changes on logic to include the uppercase, fixed the min and max of words. Also include comments on the logic.php.

--- content.php
+++ content.php
@@ -2,13 +2,16 @@
 
 <div class="container">
       <div class="header">
-        <h1 class="text-muted">CSCI-15: Flavio's Password Generator</h1>
-        <p class="lead"> The Generate Password button below will generate a random password consisting at least in four words, one number, and one symbol. 
-        	The idea is to create a strong password making it hard to guess even by brute force.
+        <h1 class="text-muted">DWA-15: Flavio's Password Generator</h1>
+        <p class="lead"> 
+        The Generate Password button below will generate a random password consisting at least in 
+        four words, one number, one symbol and one word with the first letter in uppercase.
+        The idea is to create a strong password making it hard to guess even by brute force.
         </p>
       </div>
 
       <div class="jumbotron">
+      	<h2> Your password is: <h2>
      	<h2><?php echo $password; ?></h2>
       </div>
 
@@ -16,19 +19,17 @@
       <p class="lead"> Select the options below:</p>
 	   <form method="post" action="index.php"> 
 	
-		 <label name="words">Number of words</label>
-		<select id="words" name="words">
-			<option value="1">1</option>
-        	<option value="2">2</option>
-        	<option value="3">3</option>
-        	<option value="4">4</option>
-        </select><br />
-
+	    <label name="words">Number of words</label>
+		<input type="number" name="words" value=<?=$count?> min=1 max=4 >(max=4)</br>
+		
 		<label name="number">Include a number</label>
 		<input type="checkbox" name="number" value="number"/><br />
 
 		<label name="symbol">Use a symbol</label>
 		<input type="checkbox" name="symbol" value="symbol"/><br />
+
+		<label name="symbol">First letter Uppercase</label>
+		<input type="checkbox" name="upper" value="upper"/><br />
 		
 		<input type="submit" name="submit" value="Generate your Password"/>
 
@@ -37,9 +38,8 @@
 	</div>
 	<p class='details'>		
 		<a href='http://xkcd.com/936/'>
-			<img src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>
+			<img src='images/password_strength.png' alt='xkcd style passwords'>
 		</a>
 		<br>
 	</p>
-
 </div>
--- logic.php
+++ logic.php
@@ -1,65 +1,90 @@
 <?php
 
-// Pull data from the form
+// Check how many words the user wants to use. The default is 1 (min=1 and max=4)  
 
-if (isset($_POST['words'])) {
+if (isset($_POST['words']) && $_POST['words']>0 && $_POST['words']<=4) {
   	$count = $_POST['words'];
+} else {
+
+	$count = 1;  // This is to show the default value as 1 when I first display the page.  
+	
 }
 
+// Check if user wants to use a symbol.  
 if (isset($_POST['symbol'])) {
 	$symbol = true;
 } else {
 	$symbol = false;
 }
 
+// Check if user wants to use a number. 
 if (isset($_POST['number'])) {
 	$number = true;
 } else {
 	$number = false;
 }
 
-//Dictionary
+// Check if user wants to use uppercase for all words. 
+if (isset($_POST['upper'])) {
+	$upper = true;
+} else {
+	$upper = false;
+}
 
-if ($dict = file('words.txt')){
+// This is how I can pulled the words from a file called words-linux.txt.
+// I exported a dictorary from a Linux server at /usr/share/dict/words and
+// created the file called words-linux.txt. It has 305089 words.  
+// Some of the words already have the Capital letter. 
 
-	$select_words = [];
+if ($dict = file('words-linux.txt')){
 
+	$symbols = ['!','@','#','$','%','^','&','*','?','+']; // Array of symbols
+	
+	$numbers =[0,1,2,3,4,5,6,7,8,9]; // Array of numbers 
+
+	$array_words = [];  // Array of Words
+
+// For loop to walk trought the words-linux file and push the results on the array_words.
 	for($i = 0; $i < $count; $i++)  {
 		$max = count($dict) -1;
 		$rand = rand(0,$max);
-
 		$word = $dict[$rand];
-
-		array_push($select_words, $word);
-
+		array_push($array_words, $word);
 	}
 
-// Pick a number
+// Make the first letter of each word to be uppercase.
 
-if ($number){
+	if ($upper) {
+		foreach ($array_words as $letter => $word) {
+		$array_words[$letter] = ucfirst($word);
+		}
+	}
 
-    $numbers = "0123456789";
+// This is how to find the last word on the array_word and prepare to insert a symbol if selected by user. 
+	$last = $array_words[count($array_words) - 1];
+    	
+// If number is True then it will be executed:
+	if ($number){
+		$number_max = count($symbols) - 1;
+		$number_num = rand(0, $number_max);
+		$rand_number = $numbers[$number_num];
+		$last .= $rand_number;
+ 	}
 
-	$generated_numbers = substr(str_shuffle($numbers),0,2);
+// If symbol is true then it will be executed:
+	if ($symbol){
+		$symbol_max = count($symbols) - 1;
+		$symbol_num = rand(0, $symbol_max);
+		$rand_symbol = $symbols[$symbol_num];
+		$last .= $rand_symbol;
+	}
 
- }
+//Password Outupt After check if number or symbol was added. 	
+	$array_words[count($array_words) - 1] = $last;
 
-//Pick a symbol
+// Password with the options selected. It will implode the array_words
+	$password = implode('', $array_words);
 
-if ($symbol){
-
-	$symbols = "!@#$%&*-+?=";
-
-	$generated_symbols = substr(str_shuffle($symbols),0,1);
-}
-
-//Password Outupt
-
-    $palavras = implode("",$select_words);
-
-	$password = "$generated_numbers$generated_symbols $palavras";
-
- 
 }
 
 ?>
